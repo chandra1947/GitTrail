7797269c8162894ddab266990a893371da030b2d
Infoicon Technologies <infoicon@info.local>
2018-11-20 14:43:07 +0530

    Initial Commit

---
+++ GitTrail/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitTrail
+//
+//  Created by Infoicon Technologies on 20/11/18.
+//  Copyright © 2018 Infoicon Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
