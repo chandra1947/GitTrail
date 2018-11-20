49bfc9d26e9bcbb0cf587b7bc7ad2678380f23eb
Infoicon Technologies <infoicon@Info.local>
2018-11-20 14:52:38 +0530

    2nd Time Commit by Chandra

--- GitTrail/ViewController.swift
+++ GitTrail/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Chandra Comment")
+        print("Qwerty")
     }
 
     override func didReceiveMemoryWarning() {
